f6c0b2d6475eaf8cde55b0ca0be799d0db5fef33
adib-the-noob <mdadib550@gmail.com>
2024-04-13 00:14:19 +0600

    project dockerized

--- index.js
+++ index.js
@@ -1,9 +1,57 @@
 import http from 'http';
 import express from 'express';
 import path  from 'path';
+import { Server as SocketIO } from 'socket.io';
+import { spawn } from 'child_process';
+
+const options = [
+    '-i',
+    '-',
+    '-c:v', 'libx264',
+    '-preset', 'ultrafast',
+    '-tune', 'zerolatency',
+    '-r', `${25}`,
+    '-g', `${25 * 2}`,
+    '-keyint_min', 25,
+    '-crf', '25',
+    '-pix_fmt', 'yuv420p',
+    '-sc_threshold', '0',
+    '-profile:v', 'main',
+    '-level', '3.1',
+    '-c:a', 'aac',
+    '-b:a', '128k',
+    '-ar', 128000 / 4,
+    '-f', 'flv',
+    `rtmp://a.rtmp.youtube.com/live2/<stream-key>`,
+];
 
 const app = express();
 const server = http.createServer(app);  
+const io = new SocketIO(server);
+
+const ffmpegProcess = spawn('ffmpeg', options);
+
+ffmpegProcess.stdout.on('data', (data) => {
+    console.log(`ffmpeg stdout : ${data}`);
+})
+
+ffmpegProcess.stderr.on('data', (data) => {
+    console.error(`ffmpeg error : ${data}`)
+})
+
+ffmpegProcess.on('close', (code)=>{
+    console.log(`ffmpeg process exited with code ${code}`);
+})
+
+io.on('connection', (socket) => {
+    console.log('New connection', socket.id);
+    socket.on('binarystream', stream => {
+        console.log('Binary stream incoming...');
+        ffmpegProcess.stdin.write(stream, (err) => {
+            console.log('Error', err);
+        })
+    })
+});
 
 
 
